a702c536e255ffe12bd262104467753f26007df0
Walter A <39980099+walterioo@users.noreply.github.com>
2018-11-21 22:15:28 -0700

    Added SQL connection for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require("mysql"),
+  connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
